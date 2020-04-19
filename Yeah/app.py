b1506aa2e8aeac176c41f89a9e365eb3d2722ef9
Deniss <sutugin002@gmail.com>
2020-04-19 22:25:49 +0300

    Some optimization

--- Yeah/app.py
+++ Yeah/app.py
@@ -13,15 +13,17 @@ app.secret_key = os.urandom(24)
 
 class Login(Resource):
     def post(self):
+
         json_data = request.get_json(force=True)
         un = json_data['username']
         pw = json_data['password']
+
         if un == 'admin' and pw == ';--!s@fepassw0rd':
             encoded_jwt = jwt.encode({'username': un}, 'OH HI MARK', algorithm='HS256')
             session['user'] = 'admin'
             return jsonify(token=encoded_jwt.decode("utf-8"))
-        else:
-            return jsonify(Access="Access denied")
+
+        return jsonify(Access="Access denied")
 
 
 class Palindrome(Resource):
@@ -30,16 +32,16 @@ class Palindrome(Resource):
         value = json_data['value']
         if isinstance(value, float):
             return {'Error': "Only String or Int Accepted"}
-        conn = lite.connect('Palindrome.db')
+
+        conn = connection()
         c = conn.cursor()
-        c.execute('CREATE TABLE IF NOT EXISTS palindromes(id TEXT,date TEXT,value TEXT,is_palindrome TEXT)')
         c.execute("""SELECT * FROM palindromes WHERE value = ?""", (value,))
         rows = c.fetchall()
+
         if rows is not None:
             for row in rows:
                 return {'is_palindrome': row[3]}
-        id = uuid.uuid4()
-        localtime = time.asctime(time.localtime(time.time()))
+
         if not isinstance(value, str):
             temp = value
             n = value
@@ -48,32 +50,42 @@ class Palindrome(Resource):
                 dig = n % 10
                 rev = rev * 10 + dig
                 n = n // 10
+
             if temp == rev:
-
-                c.execute("INSERT INTO palindromes(id, date, value, is_palindrome) VALUES(?,?,?,?)",
-                          (str(id), str(localtime), str(value), "True"))
-                conn.commit()
+                DatabaseRequest(generateUUID(), value, True)
                 return {'is_palindrome': True}
             else:
-
-                c.execute("INSERT INTO palindromes(id, date, value, is_palindrome) VALUES(?,?,?,?)",
-                          (str(id), str(localtime), str(value), "False"))
-                conn.commit()
+                DatabaseRequest(generateUUID(), value, False)
                 return {'is_palindrome': False}
 
+        ans = isPalindrome(value)
+        if ans == 1:
+            DatabaseRequest(generateUUID(), value, True)
+            return {'is_palindrome': True}
         else:
-            ans = isPalindrome(value)
+            DatabaseRequest(generateUUID(), value, False)
+            return {'is_palindrome': False}
 
-            if ans == 1:
-                c.execute("INSERT INTO palindromes(id, date, value, is_palindrome) VALUES(?,?,?,?)",
-                          (str(id), str(localtime), str(value), "True"))
-                conn.commit()
-                return {'is_palindrome': True}
-            else:
-                c.execute("INSERT INTO palindromes(id, date, value, is_palindrome) VALUES(?,?,?,?)",
-                          (str(id), str(localtime), str(value), "False"))
-                conn.commit()
-                return {'is_palindrome': False}
+
+
+def connection():
+    return lite.connect('Palindrome.db')
+
+def currentTime():
+    return time.asctime(time.localtime(time.time()))
+
+
+def DatabaseRequest(id, value, is_palindrome):
+    conn = connection()
+    c = conn.cursor()
+    c.execute("INSERT INTO palindromes(id, date, value, is_palindrome) VALUES(?,?,?,?)",
+            (str(id), str(currentTime()), str(value), str(is_palindrome)))
+    conn.commit()
+    conn.close()
+
+
+def generateUUID():
+    return uuid.uuid4()
 
 
 def reverse(s):
@@ -87,18 +99,17 @@ def isPalindrome(s):
     return False
 
 
-def getsession():
+def getSession():
     if 'user' in session:
         return True
     return False
 
 
 class History(Resource):
-
     def get(self):
-        conn = lite.connect('Palindrome.db')
+        conn = connection()
         c = conn.cursor()
-        c.execute('SELECT * FROM palindromes')
+        c.execute("SELECT * FROM palindromes")
         rows = c.fetchall()
 
         result = []
@@ -109,22 +120,23 @@ class History(Resource):
             send = {'id': row[0], 'date': row[1], 'value': row[2], 'is_palindrome': row[3]}
             result.append(send)
             i = i + 1
-
         return jsonify(result)
 
 
 class Delete(Resource):
     def delete(self, id):
 
-        if getsession():
-            con = lite.connect('Palindrome.db')
-            with con:
-                cur = con.cursor()
-                cur.execute("""SELECT * FROM palindromes WHERE id = ?""", (id,))
-                rows = cur.fetchone()
+        if getSession():
+            conn = connection()
+            c = conn.cursor()
+            with conn:
+                c.execute("""SELECT * FROM palindromes WHERE id = ?""", (id,))
+                rows = c.fetchone()
+
                 if rows is not None:
-                    cur.execute("""DELETE FROM palindromes WHERE id = ?""", (id,))
+                    c.execute("""DELETE FROM palindromes WHERE id = ?""", (id,))
                     return {'status': "Success"}
+
                 else:
                     return {'status': "Record does not exist"}
         else:
@@ -137,4 +149,6 @@ api.add_resource(Palindrome, '/palindrome/solve')
 api.add_resource(Login, '/login')
 
 if __name__ == '__main__':
+    c = connection()
+    c.execute('CREATE TABLE IF NOT EXISTS palindromes(id TEXT,date TEXT,value TEXT,is_palindrome TEXT)')
     app.run(debug=True)
